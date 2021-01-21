04ec90ee811e39e9c48ca6b183c4aa1344596ff1
Valentin Khaydarov <valentin.khaydarov@tu-dresden.de>
2021-01-21 01:47:36 +0100

    Update edge_node.py

--- src/edge_node.py
+++ src/edge_node.py
@@ -170,7 +170,7 @@ class EdgeNode:
         self.avg_voltage = avg_voltage
 
         del self._voltage_data[0:len(voltage_values)]
-        print self._voltage_data
+        print(self._voltage_data)
         return new_level
 
     def _run_data_collection(self):
